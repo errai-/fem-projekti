c5c58b87c98987904b0d36a659be2b17251caf3e
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-03-19 18:37:30 +0200

    Added basic loop structures to the solver

--- solver_main.m
+++ solver_main.m
@@ -2,39 +2,47 @@
 % make the mesh
 clear all;
 
-% TODO: initial circular mesh
-%mesh = ;
+addpath('./util');
 
-% Storages
-h = zeros(8,1);
+% r values to loop over
+r_vals = [10,20]';
+% k values to loop over
+k_vals = [1,2]';
+% initiating and storaging for h
+init_h = 1
+h_iters = 8;
+h_storage = zeros(h_iters,size(r_vals,1));
 
-k = 1;
-
-% System
-bilin = @(U,V,dU,dV,gX)(dU{1}.*dV{1} + dU{2}.*dV{2}-(k^2)*U.*V);
-% System edge
-bilin_edge = @(U,V,dU,dV,gX)(-1i*k*U.*V);
-% Load (could be almost anyting)
-linf = @(V,dV,gX)( (heaviside(1-gX{1}.^2-gX{2}.^2)).*V );
-
-for h_idx=1:8
-    mesh = refine_tri(mesh);
+% Domain radius loop
+for r_idx = 1:size(r_vals,1)
+    % Init circular mesh
+    mesh = discmesh(r_vals(r_idx),1);
     
-    [K,b] = simple_assembly(mesh,bilin,linf);
-    % TODO: System needs an additional path integral over the boundary edge
-    %K = K + ;
-    
-    % Evaluate the length of the longest edge in the mesh 
-    px = mesh.p(1,:); % 1xNpoint - vector
-    py = mesh.p(2,:); % 1xNpoint - vector
-
-    ex = px(mesh.edges); % 2xNedges - matrix.
-    ey = py(mesh.edges); % 2xNedges - matrix.
-
-    % evaluate the length
-    len = (ex(1,:)-ex(2,:) ).^ 2  + (ey(1,:) -ey(2,:)).^2;
-
-    h(idx) = sqrt(max(len));
+    % Mesh density loop
+    for h_idx=1:h_iters
+        mesh = refine_tri(mesh);
+        
+        % Evaluate the length of the longest edge in the mesh 
+        px = mesh.p(1,:); % 1xNpoint - vector
+        py = mesh.p(2,:); % 1xNpoint - vector
+        ex = px(mesh.edges); % 2xNedges - matrix.
+        ey = py(mesh.edges); % 2xNedges - matrix.
+        
+        % pythagoras
+        len = (ex(1,:)-ex(2,:) ).^ 2  + (ey(1,:) -ey(2,:)).^2;
+        h_storage(h_idx,r_idx) = sqrt(max(len));
+        
+        % K value loop 
+        for k = k_vals
+            % System
+            bilin = @(U,V,dU,dV,gX)(dU{1}.*dV{1} + dU{2}.*dV{2}-(k^2)*U.*V);
+            % System edge
+            bilin_edge = @(U,V,dU,dV,gX)(-1i*k*U.*V);
+        
+            [K,b] = simple_assembly(mesh,bilin,linf);
+            % TODO: System needs an additional path integral over the boundary edge
+            %K = K + ;
+        end    
+    end
 end
-
 
