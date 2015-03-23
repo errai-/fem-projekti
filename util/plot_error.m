b7c986a03be490e6e40dba1e1cbc3ce4aa7354db
samup <samu.potka@aalto.fi>
2015-03-23 13:34:44 +0200

    Typo

--- util/plot_error.m
+++ util/plot_error.m
@@ -1,7 +1,6 @@
 % Plot the H1-error
-function plot_error(mesh, k_vec, x, uexact_x, uexact_y)
-
-error = H1_error(mesh, x, uexact_x, uexact_y);
+function plot_error(k_vec, error)
 
 figure;
 plot(k_vec, error);
+hold on;
