cc66b302c0a2435ac04975589f5f01031338b998
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-03-23 16:29:50 +0200

    Minor changes

--- test_main.m
+++ test_main.m
@@ -18,11 +18,11 @@ end
 
 % Load function
 linf = @(V,dV,gX)( 0*heaviside(1-gX{1}.^2-gX{2}.^2).*V);
-b_linf = @(V,gX)( k*1i*(ones(size(gX{1}))-gX{1}./r_val).*exp(-1i*k*gX{1}));
+b_linf = @(V,gX)(k*1i*(ones(size(gX{1}))-gX{1}./r_val).*exp(-1i*k*gX{1}));
 % System
 bilin = @(U,V,dU,dV,gX)( -dU{1}.*dV{1} - dU{2}.*dV{2} + (k^2)*U.*V);
 % System edge
-b_bilin = @(U,V,gX)(1i*k*U.*V);
+b_bilin = @(U,V,gX)(-1i*k*U.*V);
 
 [K,b] = combined_assembly(mesh,bilin,linf,b_bilin,b_linf);
 
