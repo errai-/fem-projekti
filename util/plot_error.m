e78697efa045b0acb657e81cb6c48c24c6151a3e
Juho Kahala <juho.kahala@gmail.com>
2015-03-26 18:56:05 +0200

    error now plotted against h*k

--- util/plot_error.m
+++ util/plot_error.m
@@ -1,11 +1,14 @@
 % Plot the H1-error
 function plot_error(h_vec, k_vec, error, r)
 
+% multiply h-values by k-values
+h_mat = h_vec*k_vec';
+
 figure;
-loglog(h_vec, error);
+loglog(h_mat, error);
 hold on;
 title(['H1-error of a plane wave in a disk of radius ' num2str(r)]);
-xlabel('h');
+xlabel('h*k');
 ylabel('H1 error');
 for i=1:size(k_vec)
     asd{i} = num2str(k_vec(i)); 
