009efdd43fd38fc45dc4e607d835ba2a26057e2f
Juho Kahala <juho.kahala@gmail.com>
2015-03-26 19:00:30 +0200

    total_error now returns sqrt(y)

--- util/total_error.m
+++ util/total_error.m
@@ -7,3 +7,7 @@ function y = total_error(mesh,x,uexact,uexact_x,uexact_y)
 % sum L2 and H1 errors
 
 y = L2_error(mesh, x, uexact) + H1_error(mesh, x, uexact_x, uexact_y);
+
+%y = y/size(mesh.p,2);
+% take square root ?
+y=sqrt(y);
