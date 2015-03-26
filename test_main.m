ed40e97cd3ac3eb9410de75e664a4262ff5d16ce
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-03-26 16:46:33 +0200

    The sign-related issues should now be over.

--- test_main.m
+++ test_main.m
@@ -5,7 +5,7 @@ clear all;
 addpath('./util');
 
 % r value
-r_val = 10;
+r_val = 30;
 % k values to loop over
 k = 1;
 % initiating and storaging for h
@@ -17,12 +17,12 @@ for i=1:2,
 end
 
 % Load function
-linf = @(V,dV,gX)( 0*heaviside(1-gX{1}.^2-gX{2}.^2).*V);
-b_linf = @(V,gX)(k*1i*(ones(size(gX{1}))-gX{1}./r_val).*exp(-1i*k*gX{1}).*V);
+linf = @(V,dV,gX)( 1*heaviside(1-gX{1}.^2-gX{2}.^2).*V);
+b_linf = @(V,gX)(0*k*1i*(ones(size(gX{1}))-gX{1}./r_val).*exp(-1i*k*gX{1}).*V);
 % System
-bilin = @(U,V,dU,dV,gX)( -dU{1}.*dV{1} - dU{2}.*dV{2} + (k^2)*U.*V);
+bilin = @(U,V,dU,dV,gX)(dU{1}.*dV{1} + dU{2}.*dV{2} - (k^2)*U.*V);
 % System edge
-b_bilin = @(U,V,gX)(-1i*k*U.*V);
+b_bilin = @(U,V,gX)(1i*k*U.*V);
 
 [K,b] = combined_assembly(mesh,bilin,linf,b_bilin,b_linf);
 
@@ -30,6 +30,13 @@ b_bilin = @(U,V,gX)(-1i*k*U.*V);
 x = full(K\b);
 
 tri = delaunay(mesh.p(1,:)', mesh.p(2,:)');
-trisurf(tri, mesh.p(1,:)', mesh.p(2,:)', real(x));
+trisurf(tri, mesh.p(1,:)', mesh.p(2,:)', real(x),'EdgeColor','none','LineStyle','none','FaceLighting','phong');
 xlabel('X'); ylabel('Y');
+%set(gca,'XLim',[0.05 0.28],'XTick',[0.05 0.1 0.15 0.2 0.25])
+
+figure;
+tri = delaunay(mesh.p(1,:)', mesh.p(2,:)');
+trisurf(tri, mesh.p(1,:)', mesh.p(2,:)', abs(x).^2,'EdgeColor','none','LineStyle','none','FaceLighting','phong');
+xlabel('X'); ylabel('Y');
+%set(gca,'XLim',[0.05 0.28],'XTick',[0.05 0.1 0.15 0.2 0.25])
 
--- util/combined_assembly.m
+++ util/combined_assembly.m
@@ -5,6 +5,6 @@ function [K,F] = simple_assembly(mesh,bilin,linf,b_bilin,b_linf)
 [K,F] = simple_assembly(mesh,bilin,linf);
 [Ktmp,Ftmp] = boundary_assembly(mesh,b_bilin,b_linf);
 K = K+Ktmp;
-F = F+Ftmp;
+F =- F+Ftmp; % Negative F
 
 end
