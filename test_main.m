e0d704ca9340ece3440c72453c63c500d73b12b8
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-03-30 14:42:29 +0300

    Presentation soon finished.

--- test_main.m
+++ test_main.m
@@ -7,14 +7,21 @@ addpath('./util');
 % r value
 r_val = 30;
 % k values to loop over
-k = 1;
+k = 10;
 % initiating and storaging for h
-h_val = 1;
+h_val = 0.5;
 % Init circular mesh
 mesh = discmesh(r_val,h_val);
-for i=1:2,
-    mesh=refine_tri(mesh);
-end
+% Evaluate the length of the longest edge in the mesh 
+px = mesh.p(1,:);    % 1xNpoint - vector
+py = mesh.p(2,:);    % 1xNpoint - vector
+ex = px(mesh.edges); % 2xNedges - matrix.
+ey = py(mesh.edges); % 2xNedges - matrix.
+
+% pythagoras
+len = (ex(1,:)-ex(2,:) ).^ 2  + (ey(1,:) -ey(2,:)).^2;
+h_val = sqrt(max(len));
+display(h_val*k);
 
 % Load function
 linf = @(V,dV,gX)( 0*heaviside(1-gX{1}.^2-gX{2}.^2).*V);
@@ -32,7 +39,7 @@ x = full(K\b);
 tri = delaunay(mesh.p(1,:)', mesh.p(2,:)');
 trisurf(tri, mesh.p(1,:)', mesh.p(2,:)', real(x),'EdgeColor','none','LineStyle','none','FaceLighting','phong');
 xlabel('X'); ylabel('Y');
-set(gca,'XLim',[-30 30],'YLim',[-30 30],'ZLim',[-1.1,1.1]); colorbar;
+set(gca,'XLim',[-30 30],'YLim',[-30 30]); colorbar;
 
 figure;
 trisurf(tri, mesh.p(1,:)', mesh.p(2,:)', abs(x).^2,'EdgeColor','none','LineStyle','none','FaceLighting','phong');
